9a75d67bf536cdbcbc6ac6d5a39927da918f25f9
Gordon <gordon.wallac3@yahoo.com>
2017-07-12 11:52:22 -0600

    implemented edit funcitonality for the status

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,10 @@
 
 puts "1 user is HERE!"
 
+AdminUser.create(email: "test@testy.com", password: "123456", password_confirmation: "123456", first_name: "admin", last_name: "head")
+
+puts "1 admin is HERE!"
+
 30.times do |post|
 	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
