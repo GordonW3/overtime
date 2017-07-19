4c9d886a9581670124bb5a12eead4ce5c2cba137
Gordon <gordon.wallac3@yahoo.com>
2017-07-19 13:42:52 -0600

    implemented audit log to only be seen by admin users

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,7 +22,7 @@ end
 puts "MADE POSTS!!! 30 of them."
 
 30.times do |audit_log|
-	AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+	AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 end
 
 puts "STUFF HAS BEEN MADE!!! 30"
