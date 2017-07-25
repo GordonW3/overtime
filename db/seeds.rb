e7cd0a19569db56feacb090e2aa16acb81a476ff
Gordon <gordon.wallac3@yahoo.com>
2017-07-25 14:03:09 -0600

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,15 +16,13 @@ AdminUser.create(email: "test@testy.com",
 
 puts "1 admin is HERE!"
 
-30.times do |post|
-	Post.create!(date: Date.today, rationale: "#{post} rationale content of doom and distructionasdjs danasjkl d dkjdjsksk ssk sk ddk dkksa;fdlk;das j ds;sja sda;ljadfsjklfdsljsfd;jldfsajfds f. f fdjklfsdsf s fd fslsd lj sdj sdf jldf sja;j", user_id: @user.id, overtime_request: 1.5)
-end
-puts "MADE POSTS!!! 30 of them."
-
-
 AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
 AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
 
-
 puts "STUFF HAS BEEN MADE!!! 3"
+
+30.times do |post|
+	Post.create!(date: Date.today, rationale: "#{post} rationale content of doom and distructionasdjs danasjkl d dkjdjsksk ssk sk ddk dkksa;fdlk;das j ds;sja sda;ljadfsjklfdsljsfd;jldfsajfds f. f fdjklfsdsf s fd fslsd lj sdj sdf jldf sja;j", user_id: @user.id, overtime_request: 1.5)
+end
+puts "MADE POSTS!!! 30 of them."
